1ef47666419fa08ff3434e123933ea9d992e54d1
debugerr <pschuler@outlook.com>
2016-10-16 20:59:42 +0200

    fixing array parameter

--- www/CardReader.js
+++ www/CardReader.js
@@ -1,14 +1,14 @@
 var cardReaderExports = {
     init: function (successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "CardReaderPlugin", "init");
+        cordova.exec(successCallback, errorCallback, "CardReaderPlugin", "init", []);
     },
     addListener: function (callback, successCallback, errorCallback) {
         document.addEventListener("nfc_cardreader_tag", callback, false);
-        cordova.exec(successCallback, errorCallback, "CardReaderPlugin", "addListener");
+        cordova.exec(successCallback, errorCallback, "CardReaderPlugin", "addListener", []);
     },
     removeListener: function (callback, successCallback, errorCallback) {
         document.removeEventListener("nfc_cardreader_tag", callback, false);
-        cordova.exec(successCallback, errorCallback, "CardReaderPlugin", "removeListener");
+        cordova.exec(successCallback, errorCallback, "CardReaderPlugin", "removeListener", []);
     }
 };
 
